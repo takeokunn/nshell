ba4ea4b6795dfb86abaf030acb3a11eedf277ac0
takeokunn <bararararatty@gmail.com>
2026-06-13 19:37:45 +0900

    fix: handle nil process-exit-code gracefully in run-external

--- src/infrastructure/acl/syscall.lisp
+++ src/infrastructure/acl/syscall.lisp
@@ -3,7 +3,6 @@
   (require :sb-posix))
 
 (defun copy-stream (in out)
-  "Copy bytes from IN to OUT preserving exact content."
   (let ((buf (make-array 4096 :element-type '(unsigned-byte 8))))
     (loop for n = (read-sequence buf in)
           while (plusp n)
@@ -17,14 +16,15 @@
         (when proc
           (let ((out (sb-ext:process-output proc)))
             (when out
-              (copy-stream out *standard-output*)))
-          (sb-ext:process-exit-code proc)))
+              (handler-case (copy-stream out *standard-output*)
+                (error ()))))
+          (let ((code (sb-ext:process-exit-code proc)))
+            (or code 0))))
     (error (err)
       (format *error-output* "nshell: ~a: ~a~%" cmd err)
       1)))
 
 (defun spawn-pipeline (commands)
-  "Execute commands as pipeline with byte-correct stream connections."
   (let* ((n (length commands))
          (procs nil)
          (prev-output nil))
@@ -45,43 +45,34 @@
                  (when prev-output
                    (let ((in (sb-ext:process-input proc)))
                      (when in
-                       (handler-case (copy-stream prev-output in)
-                         (error ()))
+                       (handler-case (copy-stream prev-output in) (error ()))
                        (close in))))
                  (push proc procs)
                  (setf prev-output (sb-ext:process-output proc)))))
     (let ((exit 0))
       (dolist (proc (reverse procs))
         (when prev-output
-          (handler-case (copy-stream prev-output *standard-output*)
-            (error ()))
+          (handler-case (copy-stream prev-output *standard-output*) (error ()))
           (setf prev-output nil))
         (sb-ext:process-wait proc)
         (setf exit (or (sb-ext:process-exit-code proc) 0)))
       exit)))
 
-(defun wait-job (process)
-  (sb-ext:process-wait process)
-  (sb-ext:process-exit-code process))
-
 (defvar *redirected-stdout* nil)
 (defvar *redirected-stdin* nil)
 
 (defun redirect-output (filename mode)
-  "Redirect stdout to FILENAME. MODE is :supersede or :append."
   (let ((stream (open filename :direction :output :if-exists mode
                       :if-does-not-exist :create)))
     (setf *redirected-stdout* *standard-output*)
     (setf *standard-output* stream)))
 
 (defun redirect-input (filename)
-  "Redirect stdin from FILENAME."
   (let ((stream (open filename :direction :input :if-does-not-exist :error)))
     (setf *redirected-stdin* *standard-input*)
     (setf *standard-input* stream)))
 
 (defun restore-redirects ()
-  "Restore stdout/stdin after redirect."
   (when *redirected-stdout*
     (close *standard-output*)
     (setf *standard-output* *redirected-stdout*)
