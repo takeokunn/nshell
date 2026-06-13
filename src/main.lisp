780162633d99c69202f6554fc18bb81990a8c60c
takeokunn <bararararatty@gmail.com>
2026-06-13 18:11:34 +0900

    fix: use run-repl in main.lisp

--- src/main.lisp
+++ src/main.lisp
@@ -5,7 +5,7 @@
   (format t "nshell v0.1.0 - fish-inspired shell in Common Lisp (SBCL ~a)~%"
           (lisp-implementation-version))
   (handler-case
-      (nshell.presentation:run-repl-simple)
+      (nshell.presentation:run-repl)
     (error (e)
       (format *error-output* "Fatal error: ~a~%" e)
       (sb-ext:exit :code 1)))
